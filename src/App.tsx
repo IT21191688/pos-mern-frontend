6c5109a3fe2fca81ee71c255f0c0273f25358ac9
IT21191688 <IT21191688@my.sliit.lk>
2023-12-13 21:06:22 +0530

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Customer from "./components/Customer";
